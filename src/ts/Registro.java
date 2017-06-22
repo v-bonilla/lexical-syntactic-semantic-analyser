03e5f19a8ad1fb9b0f55aa7da0013fb1074d4c05
Victor Bonilla <vic.bonpar@gmail.com>
2017-06-22 13:42:13 +0200

    Add TS and main classes

---
+++ src/ts/Registro.java
@@ -0,0 +1,94 @@
+package proyecto;
+
+import java.io.PrintWriter;
+
+public class Registro {
+	private int posicion;
+	private String lexema;
+	private String tipo;
+	private int desplazamiento;
+	private int nParametros;
+	private String[] tipoParam;
+	private int idTabla;
+	private boolean funcion;
+	
+	public Registro(boolean f){
+		this.funcion = f;
+	}
+
+	public int getPosicion() {
+		return posicion;
+	}
+
+	public void setPosicion(int posicion) {
+		this.posicion = posicion;
+	}
+
+	public String getLexema() {
+		return lexema;
+	}
+
+	public void setLexema(String lexema) {
+		this.lexema = lexema;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public int getDesplazamiento() {
+		return desplazamiento;
+	}
+
+	public void setDesplazamiento(int desplazamiento) {
+		this.desplazamiento = desplazamiento;
+	}
+
+	public int getnParametros() {
+		return nParametros;
+	}
+
+	public void setnParametros(int nParametros) {
+		this.nParametros = nParametros;
+	}
+
+	public String[] getTipoParam() {
+		return tipoParam;
+	}
+
+	public void setTipoParam(String[] tipoParam) {
+		this.tipoParam = tipoParam;
+	}
+
+	public int getIdTabla() {
+		return idTabla;
+	}
+
+	public void setIdTabla(int idTabla) {
+		this.idTabla = idTabla;
+	}
+	
+	public void toString(PrintWriter pw){
+		if (this.funcion){
+			pw.println("  * (esto es una funcion) " + this.lexema);
+			pw.println("    + tipo: " + this.tipo);
+			pw.println("    + parametros: " + this.nParametros);
+			pw.println("    + idtabla: " + this.idTabla);
+			if (this.nParametros > 0){
+				for (int i = 0; i < this.tipoParam.length; i++){
+					if (tipoParam[i] != null)
+						pw.println("    + tipoparam" + i + ": " + tipoParam[i]);
+				}
+			}
+		}else{
+			pw.println("  * " + this.lexema);
+			pw.println("    + tipo: " + this.tipo);
+			pw.println("    + desplazamiento: " + this.desplazamiento);
+		}
+	}
+
+}
---
+++ src/ts/TablaSimbolos.java
@@ -0,0 +1,137 @@
+package proyecto;
+
+import java.io.PrintWriter;
+import java.util.LinkedList;
+
+public class TablaSimbolos {
+
+	private String nombre;
+	private int idTabla;
+	private int nReg;
+	private int desp;
+	private LinkedList<Registro> registros = new LinkedList<Registro>();
+
+	public TablaSimbolos(String nombre, int id){
+		this.nombre = nombre;
+		this.idTabla = id;
+		this.nReg = 0;
+		this.desp = 0;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public int getIdTabla() {
+		return idTabla;
+	}
+
+	public void setIdTabla(int idTabla) {
+		this.idTabla = idTabla;
+	}
+	
+	public void insertaLex(String lex, boolean f){
+		if (!existeLex(lex)){
+			Registro r = new Registro(f);
+			r.setLexema(lex);
+			nReg++;
+			r.setPosicion(nReg);
+			registros.add(r);
+		}
+	}
+	
+	public boolean existeLex(String lex){
+		boolean res = false;
+		if (!registros.isEmpty()){
+			for (Registro r : registros)
+				if (r.getLexema().contentEquals(lex))
+					res = true;
+		}
+		return res;
+	}
+	
+	public int buscaReg(String lex){
+		int res = -1;
+		if (!registros.isEmpty()){
+			for (Registro r : registros){
+				if (r.getLexema().contentEquals(lex)){
+					res = r.getPosicion();
+				}
+			}
+		}
+		return res;
+	}
+	
+	public void insertaTipo (String lex, String tipo){
+		if (!registros.isEmpty()){
+			for (Registro r : registros){
+				if (r.getLexema().contentEquals(lex)){
+					r.setTipo(tipo);
+					r.setDesplazamiento(this.desp);
+					if(tipo.contentEquals("int"))
+						this.desp = this.desp + 2;
+					else if(tipo.contentEquals("bool"))
+						this.desp = this.desp + 1;
+					else if(tipo.contentEquals("chars"))
+						this.desp = this.desp + 4;
+					else if(tipo.contentEquals("pal. reservada"))
+						this.desp = this.desp + 4;
+				}
+			}
+		}
+	}
+	
+	public void insertaDesp (String lex, int desp){
+		if (!registros.isEmpty()){
+			for (Registro r : registros){
+				if (r.getLexema().contentEquals(lex)){
+					r.setDesplazamiento(desp);
+				}
+			}
+		}
+	}
+	
+	public void insertaNParam (String lex, int n){
+		if (!registros.isEmpty()){
+			for (Registro r : registros){
+				if (r.getLexema().contentEquals(lex)){
+					r.setnParametros(n);
+				}
+			}
+		}
+	}
+	
+	public void insertaTipoParam (String lex, String[] t){
+		if (!registros.isEmpty()){
+			for (Registro r : registros){
+				if (r.getLexema().contentEquals(lex)){
+					r.setTipoParam(t);
+				}
+			}
+		}
+	}
+	
+	public void insertaIdTabla (String lex, int id){
+		if (!registros.isEmpty()){
+			for (Registro r : registros){
+				if (r.getLexema().contentEquals(lex)){
+					r.setIdTabla(id);
+				}
+			}
+		}
+	}
+	
+	public void toString(PrintWriter pw){
+		pw.println("Tabla " + this.nombre + " #" + this.idTabla + ":");
+		if (!registros.isEmpty()){
+			for (Registro r : registros){
+				r.toString(pw);
+			}
+		}
+	}
+
+}
